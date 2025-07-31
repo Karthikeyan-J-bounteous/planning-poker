6c4072ddbcc759ac43fb243e8ee331d1e524dd42
Karthikeyan J <karthikeyan.jothibhaskaran@bounteous.com>
2025-07-31 09:52:01 +0530

    updated qr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MenuComponent } from './menu/menu.component';
-import { QRCodeModule } from 'angularx-qrcode';
 import { reducers } from './store/reducers';
 import { metaReducers } from './store/reducers/local-storage-sync.reducer';
 import { GameEffects } from './store/effects/game.effects';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, MenuComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule, QRCodeModule, StoreModule.forRoot(reducers, { metaReducers }), EffectsModule.forRoot([GameEffects])],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule, StoreModule.forRoot(reducers, { metaReducers }), EffectsModule.forRoot([GameEffects])],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
